374658545f702a66f083ba16b4d5f87851f1e805
matias <mmedina@toth.cl>
2024-06-28 11:42:43 -0400

    mas cambios para instalador v1

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ case $1 in
 i3)
 	echo "instalando dependencias"
 	sudo pacman -Sy
-	sudo pacman -S --needed git base-devel rofi pavucontrol feh gnome-terminal nautilus blueman lxterminal network-manager-applet xfce4-screenshooter i3blocks volumeicon polkit-gnome awesome-terminal-fonts
+	sudo pacman -S --needed git curl base-devel rofi pavucontrol feh gnome-terminal nautilus blueman lxterminal network-manager-applet xfce4-screenshooter i3blocks volumeicon polkit-gnome awesome-terminal-fonts
 	mkdir -p ${HOME}/Imágenes/fondos
 	cp i3/fondo.jpg ${HOME}/Imágenes/fondos
 	#instalando pamac
