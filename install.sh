47ffdc6281b1e17400f30dca09b593b4aaa1f198
matias <mmedina@toth.cl>
2024-06-28 11:30:22 -0400

    mas cambios para instalador v1

---
+++ install.sh
@@ -0,0 +1,14 @@
+case $1 in
+i3)
+	echo "instalando dependencias"
+	sudo pacman -Sy
+	sudo pacman -S --needed git base-devel rofi pavucontrol feh gnome-terminal nautilus blueman lxterminal network-manager-applet xfce4-screenshooter i3blocks volumeicon polkit-gnome awesome-terminal-fonts
+	mkdir -p ${HOME}/Imágenes/fondos
+	cp i3/fondo.jpg ${HOME}/Imágenes/fondos
+	#instalando pamac
+	git clone https://aur.archlinux.org/pamac-all-git.git
+	/bin/bash -c "cd pamac-all-git && makepkg -sic"
+;;
+esac
+
+
