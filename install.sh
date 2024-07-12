aa90ea12426ffb86cd6fe1437aa86e6110e74749
matias <mmedina@toth.cl>
2024-07-12 11:07:44 -0400

    mas cambio

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ case $1 in
 archlinux-i3)
 	echo "instalando dependencias"
 	sudo pacman -Sy
-	sudo pacman -S --needed git curl base-devel rofi pavucontrol feh gnome-terminal nautilus blueman lxterminal network-manager-applet xfce4-screenshooter i3blocks volumeicon polkit-gnome awesome-terminal-fonts
+	sudo pacman -S --needed git curl base-devel rofi pavucontrol feh nautilus tmux blueman lxterminal network-manager-applet flameshot i3blocks polkit-gnome awesome-terminal-fonts noto-fonts-emoji
 	mkdir -p ${HOME}/Imágenes/fondos
 	cp i3/fondo.jpg ${HOME}/Imágenes/fondos
 	#instalando pamac
